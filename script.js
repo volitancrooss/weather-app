387b73c49dabb6cec8885cf966654d679ca0132f
Alexander <spaco7944@gmail.com>
2025-05-06 09:57:17 +0100

    Subida - Inicial

---
+++ script.js
@@ -0,0 +1,71 @@
+let form = document.querySelector(".section-search form");
+let input = document.querySelector(".section-search input");
+let msg = document.querySelector(".section-search .msg");
+let lista = document.querySelector(".results .ciudades");
+
+const API_KEY = "a70f90cef2e625150f48bbd1a9964621";
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let ciudad = input.value;
+    const ListaItems = lista.querySelectorAll(".results .ciudad");
+    const ListaIterable = Array.from(ListaItems);
+
+    if(ListaIterable.length > 0) {
+        const filtrado = ListaIterable.filter(el => {
+            let content = "";
+            if(ciudad.includes(",")) {
+                if(ciudad.split(",")[1].length > 2) {
+                    ciudad = ciudad.split(",")[0];
+                    content = el
+                      .querySelector(".ciudad-name span")
+                      .textContent.toLowerCase();
+                } else {
+                    content = el.querySelector(".ciudad-name span").dataset.name.toLowerCase();
+                }
+            } else {
+                content = el.querySelector(".ciudad-name span").textContent.toLowerCase();
+            }
+            return content == ciudad.toLowerCase();
+        });
+
+        if(filtrado.length > 0) {
+            msg.textContent = `Ya lo estás consultando para ${filtrado[0].querySelector(".ciudad-name span").textContent.toLowerCase()}`;
+            form.reset();
+            input.focus();
+            return;
+        }
+    }
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, sys, weather } = data;
+            const icono = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                weather[0]["icon"]
+            }.svg`;
+        
+            const li = document.createElement("li");
+            li.classList.add("ciudad");
+            const marca = `<h2 class="ciudad-name" data-name="${name},${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="ciudad-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="ciudad-icon" src="${icono}" alt="${weather[0]["descripcion"]}">
+            </figure>
+            `;
+            li.innerHTML = marca;
+            lista.appendChild(li);
+        })
+        .catch(() => {
+            msg.textContent = "No se ha encontrado la ciudad, prueba de nuevo";
+        });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
